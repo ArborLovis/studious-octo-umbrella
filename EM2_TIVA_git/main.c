aae2fed41fbea026b13b4c42ca80be2d385f38d4
Michi Teu <michael.teuschler@edu.fh-joanneum.at>
2018-10-23 20:27:35 +0200

    only important files

--- EM2_TIVA_git/main.c
+++ EM2_TIVA_git/main.c
@@ -6,13 +6,14 @@
 
 #include "tiva_headers.h"
 #include "Source_test.h"
+#include "hal_init.h"
 
 
 
 int main(void)
 {
     //Initialization HAL System
-
+    void hal_init();
 
     while(1)
     {
